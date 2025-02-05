3e87374c2ebbdcbe51670148f8020d8ab87d9562
synergisticgreed <sameergptnsr@gmail.com>
2025-02-05 18:47:46 +0530

    next commit

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -1,5 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
 
+import todoReducer from '..features/todo/todoSlice'
 
 
-export const store = configureStore({})
+
+export const store = configureStore({
+    reducer: todoReducer
+    
+})
